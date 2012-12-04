a397d8ff43d09f0abee27f6e614195f2d2a453ef
Lawrence Chan <element103@gmail.com>
2012-12-03 23:45:36 -0600

    Update monsterbar.sh to use multibar feature
    - Use tee process substitution to split into two bars

--- monsterbar.sh
+++ monsterbar.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
+set +o posix
 
 fifo="/tmp/m"
 
 trap 'rm -f "${fifo}"' INT TERM EXIT
 [ -p "${fifo}" ] || { rm -f "${fifo}"; mkfifo -m 600 "${fifo}"; }
 
-mstatusbar < "${fifo}" | bar &
+mstatusbar < "${fifo}" | tee >(bar -s 0) >(bar -s 1) >/dev/null &
 
 monsterwm > "${fifo}"
